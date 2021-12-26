5cc27fb214927aef628ff49a671c828581f24e53
Pace Willisson <pace@alum.mit.edu>
2021-12-26 18:22:42 -0500

    works

--- login.php
+++ login.php
@@ -11,6 +11,7 @@ $extra_javascript .= "<script src='login.js'></script>\n";
 
 $arg_pub = trim (@$_REQUEST['pub']);
 $arg_sig = trim (@$_REQUEST['sig']);
+$arg_cksum = intval(@$_REQUEST['cksum']);
 
 if (($nonce = @getsess ("nonce")) == "") {
     $nonce = generate_urandom_string(8);
@@ -25,11 +26,43 @@ if ($arg_pub == "") {
     pfinish ();
 }
 
-$body .= "have pub ";
-$body .= "<pre>\n";
-$body .= h($arg_pub);
-$body .= "</pre>\n";
+$body .= sprintf ("<p>cksum %d</p>\n", $arg_cksum);
 
-$sig = base64_decode ($arg_sig);
+if (0) {
+    $body .= "<pre>\n";
+    $body .= h($arg_pub);
+    $body .= "</pre>\n";
+
+    $body .= "<pre>\n";
+    $body .= h($arg_sig);
+    $body .= "</pre>\n";
+}
+
+$sig_binary = base64_decode ($arg_sig);
+
+$len = strlen($sig_binary);
+$body .= sprintf ("<p>sig len %d</p>\n", $len);
+
+$cksum = 0;
+for ($i = 0; $i < $len; $i++) {
+    $cksum += ord($sig_binary[$i]);
+}
+$body .= sprintf ("<p>cksum %d</p>\n", $cksum);
+
+$key = openssl_pkey_get_public ($arg_pub);
+
+if (0) {
+    $k = openssl_pkey_get_details($key);
+    $body .= "<pre>\n";
+    $body .= h($k['key']);
+    $body .= h($arg_pub);
+    $body .= "</pre>\n";
+}
+
+$nonce = 'x';
+var_dump ($nonce);
+$val = openssl_verify ($nonce, $sig_binary, $arg_pub, 'sha256');
+$body .= sprintf ("<p>result %d</p>\n", $val);
+
 
 pfinish ();
--- static/login.js
+++ static/login.js
@@ -1,6 +1,6 @@
 const twentyBytes = crypto.getRandomValues(new Uint8Array(20));
 
-function encode(s) {
+function base64_encode(s) {
   return (btoa(String.fromCharCode.apply(null, new Uint8Array(s))));
 }
 
@@ -19,13 +19,13 @@ async function get_key_pair () {
   let key_store = new KeyStore ();
   await key_store.open();
 
-  const key_name = "wcauth4";
+  const key_name = "wcauth5";
 
   key = await key_store.getKey("name", key_name);
   if (! key) {
     console.log ("make key");
     let key_pair = await window.crypto.subtle.generateKey({
-      name: "RSA-PSS",
+      name: "RSASSA-PKCS1-v1_5",
       modulusLength: 1024,
       publicExponent: new Uint8Array([1, 0, 1]),
       hash: "SHA-256",
@@ -38,24 +38,45 @@ async function get_key_pair () {
   return (await key_store.getKey("name", key_name));
 }
 
-
 async function wcauth_start () {
   let key_pair = await get_key_pair ();
 
   pub = await encode_public_key (key_pair.publicKey);
 
   var enc = new TextEncoder();
+
+  // force text to be a space
+  wcauth_nonce = 'x'
+
   let nonce_bytes = enc.encode(wcauth_nonce)
+
+//  nonce_bytes = new Uint8Array([0]);
+//  nonce_bytes[0] = 32;
+
+  console.log(['input', nonce_bytes]);
   let sig = await window.crypto.subtle.sign({
-    name: "RSA-PSS",
-    saltLength: 32,
+    name: "RSASSA-PKCS1-v1_5",
   }, key_pair.privateKey, nonce_bytes);
 
-  let sig_base64 = btoa (sig);
+  console.log(['sig', sig]);
+
+  let sig_view = new Uint8Array(sig);
+
+  var cksum = 0;
+  for (var i = 0; i < sig.byteLength; i++)
+    cksum += sig_view[i];
+  console.log(cksum);
+  // 16159
+
+  // 132 174 55 246 ... for 128 bytes
+  console.log(sig);
+  let sig_base64 = base64_encode (sig);
+  console.log(sig_base64);
 
   let dest = "/login.php" +
-      "?pub=" + encodeURIComponent(pub) +
-      "&sig=" + encodeURIComponent(sig_base64);
+      "?cksum=" + cksum +
+      "&sig=" + encodeURIComponent(sig_base64) +
+      "&pub=" + encodeURIComponent(pub);
 
   console.log(dest);
   window.location = dest;
